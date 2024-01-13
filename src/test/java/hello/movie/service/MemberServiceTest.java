50bc52de91c371d6b554145da13005f310589ffb
Joegaeul <whrkdmf258@naver.com>
2024-01-14 03:27:55 +0900

    Test: MemberServiceTest에서 회원가입 테스트 코드 작성

--- src/test/java/hello/movie/service/MemberServiceTest.java
+++ src/test/java/hello/movie/service/MemberServiceTest.java
@@ -3,23 +3,24 @@ package hello.movie.service;
 import hello.movie.dto.MemberDto.CreateMemberDto;
 import hello.movie.model.Member;
 import hello.movie.repository.MemberRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(SpringExtension.class)
 class MemberServiceTest {
 
-    @Mock
+    @Spy
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Mock
@@ -31,16 +32,26 @@ class MemberServiceTest {
     @Test
     void join() {
         // given
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         CreateMemberDto createMemberDto = CreateMemberDto.builder()
                 .userId("testA")
                 .password("0000")
                 .build();
+        String encodePw = encoder.encode(createMemberDto.getPassword());
+
+        doReturn(Member.builder().userId(createMemberDto.getUserId()).password(encodePw).build())
+                .when(memberRepository).save(any(Member.class));
 
         // when
         Optional<Member> member = memberService.join(createMemberDto);
 
         // then
         assertThat(member.get().getUserId()).isEqualTo(createMemberDto.getUserId());
+        assertThat(encoder.matches(createMemberDto.getPassword(), member.get().getPassword())).isTrue();
+
+        // verify
+        verify(memberRepository, times(1)).save(any(Member.class));
+        verify(bCryptPasswordEncoder, times(1)).encode(any(String.class));
     }
 
     @Test
